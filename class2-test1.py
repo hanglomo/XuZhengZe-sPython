30db38e48546ed3b465df9ae7b5c88fa591b5682
hanglomo <shhlomo@Outlook.com>
2021-05-22 08:14:30 +0800

    Create class2-test1.py
    许政泽第二节课课堂源码

---
+++ class2-test1.py
@@ -0,0 +1,22 @@
+    name="我是许政泽,"
+    mape="我今年11岁了"
+    print(name+mape)
+    name1="我要好好学习,"
+    print(name1*10)
+    print(3000000*6)
+    A=19
+    S=33
+    D=S%A
+    print(5+6==11)
+    print(D)
+    UIO=int(input("请输入鸡蛋的个数"))
+    h=0.68*UIO
+    h=h//1
+    print(h)
+    import turtle as t
+    t.shape('turtle')
+    t.pensize(15)
+    t.color('yellow')
+    t.fd(50)
+    t.color('red')
+    t.fd(150)
